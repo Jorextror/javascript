07093e9a1ad49085da00500a9439a2d292c15178
Jorextror <joliveda@gmail.com>
2021-11-26 18:24:40 +0100

    036

--- UF2/ex3_2Promises chaining/activitat/js/1promise.js
+++ UF2/ex3_2Promises chaining/activitat/js/1promise.js
@@ -10,19 +10,28 @@ function whereAmI(lat,lng) {
             fetch("https://restcountries.com/v3.1/name/"+data.country)
                 .then(data1 => data1.json())
                 .then(data1 =>{
-                    console.log(data1);
-                    document.write("<table>")
-                    document.write("<tr>")
-                    document.write("<td>"+ data1.name +"</td>")
-                    document.write("<td>"+ data1.currencies +"</td>")
-                    document.write("<td>"+ data1.languages +"</td>")
-                    document.write("<td>"+ data1.capital +"</td>")
-                    document.write("<td>"+ data1.population +"</td>")
-                    document.write("</tr>")
-                    document.write("<tr>")
-                    document.write("<td><img src=${"+ data1.flags +"}></td>")
-                    document.write("</tr>")
-                    document.write("</table>")
+                    console.log(data1)
+                    let name = data1[0].name.common
+                    let currencies= data1[0].currencies
+                    let lang = data1[0].languages[0]
+                    if (data.country == "Germany"){
+                        currencies=data1[0].currencies.EUR.name
+                        lang = data1[0].languages.deu
+                    }
+                    if  (data.country == "India"){
+                        currencies=data1[0].currencies.INR.name
+                        lang = data1[0].languages.eng
+                    }
+                    if  (data.country == "South Africa"){
+                        currencies=data1[0].currencies.ZAR.name
+                        lang = data1[0].languages.afr
+                    }
+                    let capital = data1[0].capital
+                    let population =data1[0].population
+                    let flag = data1[0].flags.png
+                    
+                    let taula=`<table><tr><td><img src=${flag}><td><td><ul><li>Pais: ${name}</li><li>Moneda: ${currencies}</li><li>Idioma: ${lang}</li><li>Capital: ${capital}</li><li>Habitans: ${population}</li><ul></td></tr></table>`
+                    document.write(taula)
                 })
         })
         //4.
@@ -32,22 +41,5 @@ function whereAmI(lat,lng) {
         });
 }
 whereAmI(52.508,13.381);
-
-// whereAmI(19.037, 72.873);
-// whereAmI(-33.933, 18.474);
-
-// PART 2
-// 6. Ara és hora d'utilitzar les dades rebudes per representar un país. Per tant, 
-// agafeu l'atribut pertinent del resultat de l'API de geocodificació i connecteu-lo a l'API de països. https://restcountries.com
-// Dades que cal mostrar del país:
-
-// 7. Mostreu la següent informació del país i detecteu els errors (podeu generar una taula de 2 columnes,  
-// amb la bandera a l’esquerra i la resta d’informació en una llista (ul) a la dreta):
-
-// Nom oficial 
-// Monedes 
-// Idiomes 
-// Bandera 
-// Capital 
-// Habitants 
-
+whereAmI(19.037,72.873);
+whereAmI(-33.933,18.474);
