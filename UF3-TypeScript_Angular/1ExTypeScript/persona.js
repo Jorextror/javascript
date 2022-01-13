1782360d355a3c51390bb9220f53e168b844157c
Jorextror <joliveda@gmail.com>
2022-01-13 19:57:36 +0100

    045

--- UF3-TypeScript_Angular/1ExTypeScript/persona.js
+++ UF3-TypeScript_Angular/1ExTypeScript/persona.js
@@ -1,3 +1,19 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//---- 1 classe persona ----
 var Persona = /** @class */ (function () {
     //constructor
     function Persona(nom, cognom) {
@@ -8,11 +24,117 @@ var Persona = /** @class */ (function () {
     Persona.prototype.setName = function (nom) {
         this.nom = nom;
     };
+    Persona.prototype.setCognom = function (cognom) {
+        this.cognom = cognom;
+    };
     Persona.prototype.getName = function () {
         return this.nom;
     };
+    Persona.prototype.getCognom = function () {
+        return this.cognom;
+    };
+    //methode
+    Persona.prototype.nomComplet = function () {
+        return this.nom + " " + this.cognom;
+    };
+    ;
     return Persona;
 }());
 var jordi = new Persona('jordi', 'oliveda');
 jordi.setName("Jordi");
-console.log(jordi.getName());
+console.log(jordi.nomComplet());
+//---- 2 array 3 persones ----
+var array2persones = [new Persona('Joe', "Doe"), new Persona('Jane', 'Dane'), new Persona('George', 'Smith')];
+function llistar(persones) {
+    persones.forEach(function (element) {
+        console.log(element.nomComplet());
+    });
+}
+llistar(array2persones);
+//---- 3 herencia ----
+var Treballador = /** @class */ (function (_super) {
+    __extends(Treballador, _super);
+    function Treballador(nom, cognom, num, sou) {
+        var _this = _super.call(this, nom, cognom) || this;
+        _this.num = num;
+        _this.sou = sou;
+        return _this;
+    }
+    //set i get
+    Treballador.prototype.setNum = function (num) {
+        this.num = num;
+    };
+    Treballador.prototype.setSou = function (sou) {
+        this.sou = sou;
+    };
+    Treballador.prototype.getNum = function () {
+        return this.num;
+    };
+    Treballador.prototype.getSou = function () {
+        return this.sou;
+    };
+    return Treballador;
+}(Persona));
+//arrays
+var t1 = new Treballador('Joe', "Doe", 1, 1200);
+var t2 = new Treballador('Jane', 'Dane', 2, 500);
+var t3 = new Treballador('George', 'Smith', 3, 800);
+var array2trebalaldos = new Array();
+array2trebalaldos.push(t1);
+array2trebalaldos.push(t2);
+array2trebalaldos.push(t3);
+//funcio suma tots els sous
+function despeses_salarials(treballadors) {
+    var total = 0;
+    treballadors.forEach(function (element) {
+        total = total + element.sou;
+    });
+    return total;
+}
+var salaritotal = despeses_salarials(array2trebalaldos);
+console.log(salaritotal);
+var Cat = /** @class */ (function () {
+    function Cat(mew) {
+        this.mew = mew;
+    }
+    Cat.prototype.eat = function () {
+        console.log("gat menja");
+    };
+    Cat.prototype.sleep = function () {
+        console.log("gat dorm");
+    };
+    return Cat;
+}());
+var Dog = /** @class */ (function () {
+    function Dog(bark) {
+        this.bark = bark;
+    }
+    Dog.prototype.eat = function () {
+        console.log("gos menja");
+    };
+    Dog.prototype.sleep = function () {
+        console.log("gos dorm");
+    };
+    return Dog;
+}());
+var Bird = /** @class */ (function () {
+    function Bird(tweet) {
+        this.tweet = tweet;
+    }
+    Bird.prototype.eat = function () {
+        console.log("pajaro menja");
+    };
+    Bird.prototype.sleep = function () {
+        console.log("pajaro dorm");
+    };
+    return Bird;
+}());
+var gat = new Cat(true);
+gat.eat();
+gat.sleep();
+var gos = new Dog(true);
+gos.eat();
+gos.sleep();
+var pajaro = new Bird(true);
+pajaro.eat();
+pajaro.sleep();
--- UF3-TypeScript_Angular/1ExTypeScript/persona.ts
+++ UF3-TypeScript_Angular/1ExTypeScript/persona.ts
@@ -1,4 +1,6 @@
+//---- 1 classe persona ----
 class Persona {
+    //propietats privades
     private nom: string;
     private cognom: string;
     //constructor
@@ -19,9 +21,117 @@ class Persona {
     public getCognom():string{
         return this.cognom;
     }
-    
-}   
-
+    //methode
+    public nomComplet():string{
+        return this.nom+" "+this.cognom;
+    };
+}
 var jordi = new Persona('jordi','oliveda');
 jordi.setName("Jordi");
-console.log(jordi.getName());
+console.log(jordi.nomComplet());
+
+//---- 2 array 3 persones ----
+var array2persones= [new Persona('Joe',"Doe"),new Persona('Jane','Dane'),new Persona('George','Smith')];
+function llistar(persones:Array<Persona>) {
+    persones.forEach(element => {
+        console.log(element.nomComplet())
+    });
+}
+llistar(array2persones)
+
+//---- 3 herencia ----
+class Treballador extends Persona{
+    public num: number;
+    public sou: number;
+    //set i get
+    public setNum(num:number):void{
+    this.num=num;
+    }
+    public setSou(sou:number):void{
+        this.sou=sou;
+        }
+    public getNum():number{
+        return this.num;
+    }
+    public getSou():number{
+        return this.sou;
+    }
+    constructor(nom:string,cognom:string, num:number, sou:number){
+        super(nom,cognom)
+        this.num = num;
+        this.sou=sou;
+    }
+}
+//arrays
+var t1:Treballador=new Treballador('Joe',"Doe",1,1200);
+var t2:Treballador=new Treballador('Jane','Dane',2,500);
+var t3:Treballador=new Treballador('George','Smith',3,800);
+var array2trebalaldos: Treballador[]= new Array();
+array2trebalaldos.push(t1);
+array2trebalaldos.push(t2);
+array2trebalaldos.push(t3);
+//funcio suma tots els sous
+function despeses_salarials(treballadors:Treballador[]){
+    var total=0;
+    treballadors.forEach(element => {
+        total=total+ element.sou;
+    });
+    return total;
+}
+var salaritotal=despeses_salarials(array2trebalaldos);
+console.log(salaritotal);
+
+//---- 4 interface ----
+interface pet{
+    eat():void;
+    sleep():void;
+}
+
+class Cat implements pet{
+    private mew :boolean;
+
+    constructor(mew :boolean){
+        this.mew = mew;
+    }
+    public eat(){
+        console.log("gat menja")
+    }
+    public sleep(): void {
+        console.log("gat dorm")
+    }
+}
+class Dog implements pet{
+    private bark  :boolean;
+
+    constructor(bark  :boolean){
+        this.bark = bark;
+    }
+    public eat(){
+        console.log("gos menja")
+    }
+    public sleep(): void {
+        console.log("gos dorm")
+    }
+}
+class Bird implements pet{
+    private tweet:boolean;
+
+    constructor(tweet :boolean){
+        this.tweet = tweet;
+    }
+    public eat(){
+        console.log("pajaro menja")
+    }
+    public sleep(): void {
+        console.log("pajaro dorm")
+    }
+}
+var gat=new Cat(true);
+gat.eat();
+gat.sleep();
+var gos=new Dog(true);
+gos.eat();
+gos.sleep();
+var pajaro=new Bird(true);
+pajaro.eat();
+pajaro.sleep();
